92a451eb02e206d7756672495ded067a501ee6f8
125054556-cod <125054556@upq.edu.mx>
2026-09-18 17:24:41 -0600

    Practica en clase  php+html+css

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+    echo "Hola Crayola"
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            color: blue;
+        }
+        .seccion{
+            padding: 20px 40px;
+            margin: 20px;
+            background-color: green;
+            border: 10px hidden black;
+        }
+        .seccion2{
+            padding-top:20px;
+            padding-right: 40px;
+            padding-bottom: 20px;
+            padding-left:  40px;
+            background-color: pink;
+            margin-top:20px;
+            margin-right: 30px;
+            margin-bottom: 50px;
+            margin-left:  40px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola Crayola</h1>
+    <h2>Hola Crayola</h2>
+    <h3>Hola Crayola</h3>
+    <h4>Hola Crayola</h4>
+    <h5>Hola Crayola</h5>
+    <h6>Hola Crayola</h6>
+    <div class="seccion">
+        <h2>Titulo del parrafo</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Ducimus autem soluta iure accusantium.
+            Aut animi id inventore eos totam a officia at obcaecati 
+            esse et! Necessitatibus atque ipsum blanditiis iste?</p>
+    </div>
+     <div class="seccion2">
+        <h2>Titulo del parrafo</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Ducimus autem soluta iure accusantium.
+            Aut animi id inventore eos totam a officia at obcaecati 
+            esse et! Necessitatibus atque ipsum blanditiis iste?</p>
+    </div>
+</body>
+</html>
